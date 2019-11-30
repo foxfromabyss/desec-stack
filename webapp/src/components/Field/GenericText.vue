407012eb56968d49002e013eebd45830eda60849
Nils Wisiol <mail@nils-wisiol.de>
2019-11-30 13:57:19 +0100

    wip: re-add old development files for DNS console

---
+++ webapp/src/components/Field/GenericText.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-text-field
+    :label="label"
+    :value="value"
+    :type="type || ''"
+    placeholder="(optional)"
+    @input="input($event)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'GenericText',
+  props: {
+    label: {
+      type: String,
+      required: false,
+    },
+    value: {
+      type: [String, Number],
+      required: true,
+    },
+    type: {
+      type: String,
+      required: false,
+    },
+  },
+  methods: {
+    input(event) {
+      this.$emit('input', event);
+    },
+  },
+};
+</script>
